1ef1b602982a7359d45b5ea1a7f7d1c2e5bf89a7
zackad <zackad.dev@gmail.com>
2024-08-04 13:30:07 +0700

    test: fix url path for test target

--- smoke.test.js
+++ smoke.test.js
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test'
 // Just a _very_ basic check to verify that input + output are working
 test('ensure that basic formatting is not broken', async ({ page }) => {
   await page.goto(
-    'http://localhost:4173/prettier-php-playground/#N4IgDgTgpgLjCWUIHkwIPYDsDOIBcok8mMA6vACYwAW+AHAAwA0IMAhgEblW14AsLAK7YoAFU648AMzYAbES2zEA5rKgBFQehhR8M+VBZhqYAGpIlWfCACsAOj4gWMCG3iyVAYXQBbH2wAFAAkA-BdBQxAOVwBjKABlGABPNWswJABaGNwWaABHQXhoANdlfz05BRBiEQgYErYytgqDAF8WKAp4GHQUNHgsSVA2bBgWkXbqzDBBMbwQAB4AfmMwAB1MDYA3NggAfQpBHzAACgACAEYzgEoAbhBWoA'
+    'http://localhost:4173/prettier-twig-playground/#N4IgDgTgpgLjCWUIHkwIPYDsDOIBcok8mMA6vACYwAW+AHAAwA0IMAhgEblW14AsLAK7YoAFU648AMzYAbES2zEA5rKgBFQehhR8M+VBZhqYAGpIlWfCACsAOj4gWMCG3iyVAYXQBbH2wAFAAkA-BdBQxAOVwBjKABlGABPNWswJABaGNwWaABHQXhoANdlfz05BRBiEQgYErYytgqDAF8WKAp4GHQUNHgsSVA2bBgWkXbqzDBBMbwQAB4AfmMwAB1MDYA3NggAfQpBHzAACgACAEYzgEoAbhBWoA'
   )
 
   let title = page.locator('.layout__output')
